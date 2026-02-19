734479e5a5d5164116526a3dd234f761b5dcca2c
Sasha Fernandez <sasfernandez198@gmail.com>
2026-02-19 23:59:09 +0000

    Perfil de Usuario: Agrego diseño Bootstrap para el perfil

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,24 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+    <div class="cointainer-fluid px-4 px-md-5 mt-3">
+        <div class="d-flex justify-content-between align-items-center mb-4">
+            <h2 class="fw-bold">Perfil</h2>
+        </div>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
+        <div class="row bg-white p-5 rounded-4 justify-content-evenly row-gap-5" >
+            <div class="shadow rounded-4 p-4 col-md-5">
+                @include('profile.partials.update-profile-information-form')
+
             </div>
+            <div class="shadow rounded-4 p-4 col-md-5">
+                @include('profile.partials.update-password-form')
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
             </div>
+            <div class="shadow rounded-4 p-4">
+                @include('profile.partials.delete-user-form')
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
             </div>
         </div>
+
     </div>
+    
 </x-app-layout>
